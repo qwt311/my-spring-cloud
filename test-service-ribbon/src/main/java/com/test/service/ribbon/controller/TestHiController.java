3c5736c778579065f7cae495bb35755c0901157e
乔文涛 <wentao.qiao@lanmaoly.com>
2019-12-09 18:20:44 +0800

    init my spring cloud

---
+++ test-service-ribbon/src/main/java/com/test/service/ribbon/controller/TestHiController.java
@@ -0,0 +1,25 @@
+package com.test.service.ribbon.controller;
+
+import com.test.service.ribbon.service.TestHiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestHiController {
+
+    @Autowired
+    TestHiService testHiService;
+
+    @GetMapping(value = "/time")
+    public String time(@RequestParam Long time){
+        return testHiService.time(time);
+    }
+
+    @GetMapping(value = "/hi")
+    public String hiService(@RequestParam String name){
+        return testHiService.hiService(name);
+    }
+
+}
